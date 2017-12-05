d37b12045cc65791ae690154ca56982a468e1668
Minh Phan <wofi.minh@1pac.vn>
2017-12-06 00:46:11 +0700

    Correct username for seed

--- db/seed.rb
+++ db/seed.rb
@@ -2,7 +2,7 @@
 
 require_relative '../boot'
 
-User.create(username: '@minh', password: 'gamegame', email: 'pnminh2004@gmail.com')
+User.create(username: 'minh', password: 'gamegame', email: 'pnminh2004@gmail.com')
 
 Build.import(%i[user_id slug title total_price cpu_type price_showed provider_showed created_at updated_at], [
                [1, 'cau-hinh-intel-7tr-giai-tri-nhe-nhang', 'Cấu hình Intel 7tr giải trí nhẹ nhàng', 12000000, 'intel', true, false, Time.now, Time.now],
